c096513d8e26bbb91ea1f43ebcf284b23924ec9c
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-04-30 08:52:50 -0400

    added index.js to seeder / nodemon to package.json

---
+++ server/Seeder/index.js
@@ -0,0 +1,31 @@
+const db = require('../config/connection');
+const { User, Pet } = require('../models');
+const petseeds = require('./petseeds.json');
+
+
+db.once('open', async () => {
+  try {
+    await Pet.deleteMany({});
+    // await User.deleteMany({});
+    await Pet.create(petseeds);
+    // await User.create(userSeeds);
+
+    // for (let i = 0; i < thoughtSeeds.length; i++) {
+    //   const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
+    //   const user = await User.findOneAndUpdate(
+    //     { username: thoughtAuthor },
+    //     {
+    //       $addToSet: {
+    //         thoughts: _id,
+    //       },
+    //     }
+    //   );
+    // }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log('all done!');
+  process.exit(0);
+});
